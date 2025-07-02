264f856c24cae2c57d57aeeb7f697f547ce29c4f
Slade <saib6064@gmail.com>
2025-07-02 15:41:39 +0700

    feat: add main router and middleware for JSON parsing
    This commit introduces a main router to handle API endpoints
    and adds middleware for parsing JSON request bodies. This
    enhances the application's routing capabilities and enables
    it to process JSON data in requests.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import mainRouter from "./routes/index.route";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api", mainRouter);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
