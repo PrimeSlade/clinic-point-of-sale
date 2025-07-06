65a4447fdc4f6057dab506d0bb7991e233e7b652
Slade <saib6064@gmail.com>
2025-07-07 01:17:28 +0700

    feat: add dotenv package for environment variables feat: import and configure dotenv in index.ts feat: implement mainRouter to handle /api endpoint feat: implement errorHandler middleware feat: implement location routes feat: delete server.ts file

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import mainRouter from "./routes/index.route";
+import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler";
+
+dotenv.config();
+const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", mainRouter);
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
+
+app.use(errorHandler);
 
 export default app;
--- apps/api/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from ".";
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
