69a779ab22823553240fce52734b20e50e1e8c23
Slade <saib6064@gmail.com>
2025-07-07 01:15:24 +0700

    feat(item.controller.ts): add item controller logic
    This commit adds the logic for the item controller, including:
    - Adding a new item
    - Getting all items
    - Getting a single item by ID
    - Updating an item
    - Deleting an item
    - Added error handling for each of the above operations

--- apps/api/src/controllers/item.controller.ts
+++ apps/api/src/controllers/item.controller.ts
@@ -1,37 +1,24 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as itemModel from "../models/item.model";
+import { CustomError } from "../errors/CustomError";
 
-const addItem = async (req: Request, res: Response): Promise<void> => {
-  const { units, ...item } = req.body;
-  console.log(item);
-  console.log(units);
+const addItem = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { itemUnits, ...item } = req.body;
 
-  res.json(item);
+  if (!item && !itemUnits) {
+    throw new CustomError("Item not found", 404);
+  }
 
-  // if (!item) {
-  //   res.status(400).json({
-  //     success: false,
-  //     message: "Request body cannot be empty",
-  //   });
-  //   return;
-  // }
-
-  // try {
-  //   const addedItem = await itemModel.addItem(item);
-
-  //   res.status(200).json({
-  //     success: true,
-  //     message: "Item created",
-  //     data: addedItem,
-  //   });
-  // } catch (error: any) {
-  //   console.log(error);
-  //   res.status(500).json({
-  //     success: false,
-  //     message: "Failed to create item",
-  //     error: error.message || "Internal server error",
-  //   });
-  // }
+  try {
+    console.log(itemUnits);
+    res.json(item);
+  } catch (error: any) {
+    next(error);
+  }
 };
 
 const getItems = async (req: Request, res: Response): Promise<void> => {
@@ -87,7 +74,7 @@ const updateItem = async (req: Request, res: Response): Promise<void> => {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: "Failed to update items",
+      message: "Failed to update item",
       error: error.message || "Internal server error",
     });
   }
