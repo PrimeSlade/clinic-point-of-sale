0bef6cca268c89c35c8c321ce75167ffe78317a7
Slade <saib6064@gmail.com>
2025-07-07 01:16:43 +0700

    feat: implement location model with CRUD operations
    This commit introduces the location model with the following
    functionalities:
    - addLocation: Adds a new location to the database.
    - getAllLocations: Retrieves all locations from the database.
    - updateLocation: Updates an existing location in the database.
    - deleteLocation: Deletes a location from the database.

---
+++ apps/api/src/models/location.model.ts
@@ -0,0 +1,85 @@
+import prisma from "../config/prisma.client";
+import { CustomError } from "../errors/CustomError";
+import { NotFoundError } from "../errors/NotFoundError";
+import { Location } from "../types/location.type";
+
+const addLocation = async (data: Location) => {
+  const addedLocation = await prisma.location.create({
+    data: {
+      name: data.name,
+      address: data.address,
+      phoneNumber: {
+        create: {
+          number: data.phoneNumber,
+        },
+      },
+    },
+    include: {
+      phoneNumber: true,
+    },
+  });
+
+  return addedLocation;
+};
+
+const getAllLocations = async () => {
+  const locations = await prisma.location.findMany({
+    include: {
+      phoneNumber: true,
+    },
+    orderBy: {
+      id: "asc",
+    },
+  });
+  return locations;
+};
+
+const updateLocation = async (data: Location, id: number) => {
+  const updateData: any = {};
+
+  if (data.name !== undefined) updateData.name = data.name;
+  if (data.address !== undefined) updateData.address = data.address;
+  if (data.phoneNumber !== undefined) {
+    updateData.phoneNumber = {
+      update: {
+        number: data.phoneNumber,
+      },
+    };
+  }
+
+  const updated = await prisma.location.update({
+    where: {
+      id,
+    },
+    data: updateData,
+    include: {
+      phoneNumber: true,
+    },
+  });
+
+  return updated;
+};
+
+const deleteLocation = async (id: number) => {
+  const location = await prisma.location.findUnique({
+    where: {
+      id,
+    },
+    include: { phoneNumber: true },
+  });
+
+  if (!location) throw new NotFoundError();
+
+  await prisma.$transaction([
+    prisma.location.delete({ where: { id } }),
+    prisma.phoneNumber.delete({
+      where: {
+        id: location?.phoneNumberId,
+      },
+    }),
+  ]);
+
+  return location;
+};
+
+export { addLocation, getAllLocations, updateLocation, deleteLocation };
