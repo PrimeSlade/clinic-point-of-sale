e7ae375377a9f6005e151fe5a82cd56a0b315fbd
Slade <saib6064@gmail.com>
2025-07-07 01:15:46 +0700

    feat(item.model.ts): implement addItem function to add items to db

--- apps/api/src/models/item.model.ts
+++ apps/api/src/models/item.model.ts
@@ -1,6 +1,18 @@
-import { Item } from "../types/items.types";
+import prisma from "../config/prisma.client";
+import { UnitType } from "../generated/prisma";
+import { Item } from "../types/item.type";
 
-const addItem = async (items: Item) => {};
+const addItem = async (items: Item, unit: Array<UnitType>) => {
+  //   const addedItem = await prisma.item.create({
+  //     data: {
+  //       name: items.name,
+  //       category: items.category,
+  //       expiryDate: items.expiryDate,
+  //       description: items?.description,
+  //       pricePercent: items.pricePercent,
+  //     },
+  //   });
+};
 
 const getItems = async () => {};
 
