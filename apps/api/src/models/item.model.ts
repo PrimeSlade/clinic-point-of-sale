b3ca87d95b2810e614a09e93a8e9b2925ec91e70
Slade <saib6064@gmail.com>
2025-07-08 19:46:01 +0700

    feat(models): implement prisma models for item

--- apps/api/src/models/item.model.ts
+++ apps/api/src/models/item.model.ts
@@ -1,20 +1,122 @@
 import prisma from "../config/prisma.client";
-import { UnitType } from "../generated/prisma";
-import { Item } from "../types/item.type";
+import { CustomError } from "../errors/CustomError";
+import { NotFoundError } from "../errors/NotFoundError";
+import { Item, Unit, UpdateItem, UpdateUnit } from "../types/item.type";
 
-const addItem = async (items: Item, unit: Array<UnitType>) => {
-  //   const addedItem = await prisma.item.create({
-  //     data: {
-  //       name: items.name,
-  //       category: items.category,
-  //       expiryDate: items.expiryDate,
-  //       description: items?.description,
-  //       pricePercent: items.pricePercent,
-  //     },
-  //   });
+const addItem = async (data: Item, unit: Array<Unit>) => {
+  try {
+    const addedItem = await prisma.item.create({
+      data: {
+        name: data.name,
+        category: data.category,
+        expiryDate: data.expiryDate,
+        description: data?.description,
+        pricePercent: data.pricePercent,
+        locationId: data.locationId,
+        itemUnits: {
+          createMany: {
+            data: unit.map((u) => ({
+              unitType: u.unitType,
+              unit: u.unit,
+              purchasePrice: u.purchasePrice,
+            })),
+          },
+        },
+      },
+      include: {
+        location: true,
+        itemUnits: true,
+      },
+    });
+
+    return addedItem;
+  } catch (error: any) {
+    if (error.code === "P2025") {
+      throw new NotFoundError();
+    }
+    console.error(error);
+    throw new CustomError("Database operation failed", 500);
+  }
 };
 
-const getItems = async () => {};
+const getItems = async () => {
+  const items = await prisma.item.findMany({
+    include: {
+      location: true,
+      itemUnits: true,
+    },
+    orderBy: {
+      id: "asc",
+    },
+  });
 
-const updateItem = async (items: any, id: number) => {};
-export { addItem, getItems, updateItem };
+  return items;
+};
+
+const updateItem = async (
+  data: UpdateItem,
+  unit: Array<UpdateUnit>,
+  id: number,
+) => {
+  try {
+    const updated = await prisma.item.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: data.name,
+        category: data.category,
+        expiryDate: data.expiryDate,
+        description: data?.description,
+        pricePercent: data.pricePercent,
+        locationId: data.locationId,
+        itemUnits: {
+          update: unit.map((u) => ({
+            where: { id: u.id },
+            data: {
+              unitType: u.unitType,
+              unit: u.unit,
+              purchasePrice: u.purchasePrice,
+            },
+          })),
+        },
+      },
+      include: {
+        location: true,
+        itemUnits: true,
+      },
+    });
+
+    return updated;
+  } catch (error: any) {
+    if (error.code === "P2025") {
+      throw new NotFoundError();
+    }
+    console.error(error);
+    throw new CustomError("Database operation failed", 500);
+  }
+};
+
+const deleteItem = async (id: number) => {
+  try {
+    const deleted = await prisma.item.delete({
+      where: {
+        id,
+      },
+      include: {
+        location: true,
+        itemUnits: true,
+      },
+    });
+
+    return deleted;
+  } catch (error: any) {
+    if (error.code === "P2025") {
+      throw new NotFoundError();
+    }
+    console.error(error);
+    throw new CustomError("Database operation failed", 500);
+  }
+};
+
+export { addItem, getItems, updateItem, deleteItem };
