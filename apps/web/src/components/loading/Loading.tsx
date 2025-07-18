9b601d67094975f45fd038831d85d047f90c6e8c
Slade <saib6064@gmail.com>
2025-07-18 20:37:40 +0700

    feat(component): implement Loading component

---
+++ apps/web/src/components/loading/Loading.tsx
@@ -0,0 +1,11 @@
+import type { LoadingProps } from "@/types/LoadingType";
+
+const Loading = ({ className }: LoadingProps) => {
+  return (
+    <div className={`flex justify-center items-center ${className}`}>
+      <div className="w-10 h-10 border-4 border-t-transparent border-[var(--primary-color)] rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Loading;
