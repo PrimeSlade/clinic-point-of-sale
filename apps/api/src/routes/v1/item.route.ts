79d257d7a3e1613ca410cbee09ea86f990fca70f
Slade <saib6064@gmail.com>
2025-08-18 01:24:20 +0700

    feat(item.route.ts): add authorization middleware to GET '/' route

--- apps/api/src/routes/v1/item.route.ts
+++ apps/api/src/routes/v1/item.route.ts
@@ -5,7 +5,7 @@ import authorize from "../../abilities/authorize.middleware";
 const router = Router();
 
 router.post("/add", authorize("create", "Item"), itemController.addItem);
-router.get("/", itemController.getItems); //authorize("read", "Item"),
+router.get("/", authorize("read", "Item"), itemController.getItems);
 router.get("/:id", authorize("read", "Item"), itemController.getItemById);
 router.put("/:id", authorize("update", "Item"), itemController.updateItem);
 router.delete("/:id", authorize("delete", "Item"), itemController.deleteItem);
