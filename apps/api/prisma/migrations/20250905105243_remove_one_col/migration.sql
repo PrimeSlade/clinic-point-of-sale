85959bccad3affe336714ac2f6effd72e49f2945
Slade <saib6064@gmail.com>
2025-09-06 14:31:27 +0700

    feat: add invoice_services table and remove quantity column
    This commit introduces a new table `invoice_services` to store
    information about services included in invoices. It also removes the
    `quantity` column from this table.
    
    The changes include:
    
    - Creation of the `invoice_services` table with fields for id, name,
      retail price, invoice ID, and service ID.
    - Removal of the `quantity` column from the `invoice_services` table.
    - Modification of the Prisma schema to reflect these changes, including
      adding relations between `InvoiceService`, `Invoice`, and `Service`
      models.
    - Renaming column purchase_price to retail_price in invoice_items table

---
+++ apps/api/prisma/migrations/20250905105243_remove_one_col/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quantity` on the `invoice_services` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "invoice_services" DROP COLUMN "quantity";
