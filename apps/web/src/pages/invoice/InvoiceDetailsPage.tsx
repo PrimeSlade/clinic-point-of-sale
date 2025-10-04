c83355b472a677428ea6ca9334b1666399c17b0d
Slade <saib6064@gmail.com>
2025-10-04 14:43:22 +0700

    feat(InvoiceDetailsPage): implement print functionality using iframe for better user experience

--- apps/web/src/pages/invoice/InvoiceDetailsPage.tsx
+++ apps/web/src/pages/invoice/InvoiceDetailsPage.tsx
@@ -3,22 +3,13 @@ import InvoicePreview from "@/components/invoice/InvoicePreview";
 import Loading from "@/components/loading/Loading";
 import { Button } from "@/components/ui/button";
 import { useInvoice } from "@/hooks/useInvoices";
-import {
-  BlobProvider,
-  pdf,
-  PDFDownloadLink,
-  PDFViewer,
-  usePDF,
-} from "@react-pdf/renderer";
-import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { usePDF } from "@react-pdf/renderer";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const InvoiceDetailsPage = () => {
   const { id } = useParams();
-  const [errorOpen, setErrorOpen] = useState(false);
-
   const navigate = useNavigate();
-
   const {
     data: invoice,
     isLoading: isFetchingInvoice,
@@ -35,12 +26,30 @@ const InvoiceDetailsPage = () => {
     }
   }, [invoice?.data]);
 
+  const handlePrint = async () => {
+    // Create iframe for printing
+    if (instance.url) {
+      const iframe = document.createElement("iframe");
+      iframe.style.display = "none";
+      iframe.src = instance.url;
+      document.body.appendChild(iframe);
+
+      iframe.onload = () => {
+        iframe.contentWindow?.print();
+
+        // // Clean up after print
+        iframe.contentWindow?.addEventListener("afterprint", () => {
+          document.body.removeChild(iframe);
+        });
+      };
+    }
+  };
+
   if (isFetchingInvoice)
     return <Loading className="flex justify-center h-screen items-center" />;
 
-  if (instance.loading) return <div>Loading...</div>;
-
-  if (!instance.url) return <div>Error generating PDF</div>;
+  if (instance.loading) return <div>Loading PDF...</div>;
+  if (instance.error) return <div>Error generating PDF: {instance.error}</div>;
 
   return (
     <div className="m-20">
@@ -52,8 +61,11 @@ const InvoiceDetailsPage = () => {
         >
           Back
         </Button>
-        <Button className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-md hover:bg-[var(--primary-color-hover)]">
-          <a href={instance.url}>Print PDF</a>
+        <Button
+          className="px-4 py-2 bg-[var(--primary-color)] text-white rounded-md hover:bg-[var(--primary-color-hover)]"
+          onClick={handlePrint}
+        >
+          Print PDF
         </Button>
       </div>
     </div>
